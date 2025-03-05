829eff6cfbab0dbe983acb626940586db69ce240
GOROman <yoshihito.kondoh@gmail.com>
2025-03-05 18:48:58 +0900

    ドキュメントとコメント整理(by Cline)

--- README.md
+++ README.md
@@ -1,17 +1,34 @@
 # M5Module-Fan-Test
-M5Stack Fan Module v1.1 (STM32F030) Test
+M5Stack Fan Module v1.1 (STM32F030) テストプログラム
 
 ## 概要
-このプロジェクトは、M5Stack Fan Module v1.1 (STM32F030)をテストするためのPlatformIOプロジェクトです。M5Unifiedライブラリを使用して、ファンモジュールの制御を行います。
+このプロジェクトは、M5Stack Fan Module v1.1 (STM32F030)を制御・テストするためのPlatformIOプロジェクトです。M5Unifiedライブラリを使用して、FANモジュールの様々な機能を制御し、動作状態をリアルタイムで表示します。
 
 ## 機能
-- 現在の速度表示
-- ファンモジュールのバージョン表示
+- ファンの動作状態（稼働/停止）の表示と制御
+- PWM周波数の表示（1KHz/12KHz/24KHz/48KHz）
+- PWMデューティサイクル（0-100%）の表示と調整
+- 現在の回転数（RPM）のリアルタイム表示
 
+## 操作方法
+- ボタンA：ファンの稼働/停止を切り替え
+- ボタンB：デューティサイクルを10%ずつ減少（0%になると100%に戻る）
+- ボタンC：デューティサイクルを10%ずつ増加（100%になると0%に戻る）
+
+## 必要なハードウェア
+- M5Stack Core S3（または互換機）
+- M5Stack Fan Module v1.1 (STM32F030)
 
 ## 必要なライブラリ
-- M5Unified
-- M5Module-Fan
+- [M5Unified](https://github.com/m5stack/M5Unified)
+- [M5Module-Fan](https://github.com/m5stack/M5Module-Fan)
 
 ## 接続方法
-M5Stack本体のGroveポート（I2C）にFan Moduleを接続します。
+M5Stack Fan ModuleをM5Stack Core S3（または互換機）のポートに接続します。このプログラムではWire1（I2C）を使用し、SCLはピン12、SDAはピン11に設定されています。
+
+## 技術的な詳細
+- I2Cアドレス：MODULE_FAN_BASE_ADDR（デフォルト）
+- I2C速度：400kHz
+- デフォルトPWM周波数：1KHz
+- デフォルトデューティサイクル：80%
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,29 @@
+/**
+ * @file main.cpp
+ * @brief M5Stack用ファンモジュール制御プログラム
+ * @details M5Stackを使用してファンモジュールの回転速度や動作状態を制御・表示するプログラムです
+ * @author M5Stack
+ * @date 2025/03/05
+ */
+
 #include <M5Unified.h>
 #include <m5_module_fan.hpp>
 
+/// @brief ファンモジュールのインスタンス
 M5ModuleFan moduleFan;
+/// @brief ファンモジュールのI2Cアドレス
 uint8_t deviceAddr = MODULE_FAN_BASE_ADDR;
+/// @brief ファンのデューティサイクル（0-100%）
 uint8_t dutyCycle  = 80;
+/// @brief ファンの動作状態（true: 動作中、false: 停止中）
 bool fan_status    = true;
+/// @brief 表示用キャンバス
 M5Canvas canvas(&M5.Display);
 
+/**
+ * @brief 初期化処理を行う関数
+ * @details M5Stackの初期化、シリアル通信の設定、ディスプレイの設定、ファンモジュールの初期化を行います
+ */
 void setup()
 {
     M5.begin();
@@ -29,11 +46,21 @@ void setup()
     moduleFan.setStatus(MODULE_FAN_ENABLE);
 }
 
+/**
+ * @brief メインループ処理を行う関数
+ * @details ファンの状態を表示し、ボタン入力に応じてファンの動作を制御します
+ * - ボタンA: ファンのON/OFF切り替え
+ * - ボタンB: デューティサイクルを10%ずつ減少
+ * - ボタンC: デューティサイクルを10%ずつ増加
+ */
 void loop()
 {
+    /// @brief 現在のPWM周波数設定インデックス
     int pwm_config_index  = moduleFan.getPWMFrequency();
+    /// @brief PWM周波数の文字列表現
     String pwm_config_str = "";
 
+    // PWM周波数インデックスを文字列に変換
     switch (pwm_config_index) {
         case PWM_1KHZ:
             pwm_config_str = "1KHz";
@@ -56,8 +83,10 @@ void loop()
     canvas.printf("RPM            :%drpm\r\n", moduleFan.getRPM());
     canvas.pushSprite(0, 0);
 
+    // ボタン状態の更新
     M5.update();
     auto t = M5.Touch.getDetail();
+    // ボタンAが押された場合：ファンのON/OFF切り替え
     if (M5.BtnA.wasPressed()) {
         fan_status = !fan_status;
         if (fan_status) {
@@ -66,6 +95,7 @@ void loop()
             moduleFan.setStatus(MODULE_FAN_DISABLE);
         }
     }
+    // ボタンBが押された場合：デューティサイクルを10%ずつ減少
     if (M5.BtnB.wasPressed()) {
         dutyCycle -= 10;
         if (dutyCycle < 0) {
@@ -73,6 +103,7 @@ void loop()
         }
         moduleFan.setPWMDutyCycle(dutyCycle);
     }
+    // ボタンCが押された場合：デューティサイクルを10%ずつ増加
     if (M5.BtnC.wasPressed()) {
         dutyCycle += 10;
         if (dutyCycle > 100) {
